54a39161cab42a6e1f8e15b9af4de6d0096657dc
K_B <fengkunbin@126.com>
2018-03-12 22:19:32 +0800

    10b User profiles

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
 	FLASKY_MAIL_SENDER = 'fengkunbin@126.com'
-	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+	FLASKY_ADMIN = 'fengkunbin@126.com'#os.environ.get('FLASKY_ADMIN')
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 	@staticmethod
