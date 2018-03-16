4f4078770def930dda2d154d0c004b6ce00b4fb1
K_B <fengkunbin@126.com>
2018-03-16 11:15:30 +0800

    11c d Generate fake users and posts, Blog post pagination

--- config.py
+++ config.py
@@ -18,6 +18,7 @@ class DevelopmentConfig(Config):
 	MAIL_SERVER = 'smtp.126.com'
 	MAIL_PORT = 25
 #	MAIL_USE_SSL = True
+	FLASKY_POSTS_PER_PAGE = 18
 	MAIL_USERNAME = 'fengkunbin@126.com'#os.environ.get('MAIL_USERNAME')#
 	MAIL_PASSWORD = '126mail'#os.environ.get('MAIL_PASSWORD')#
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
