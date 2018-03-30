096b8a2ad10be5bbd3a5497c2efa75c599eb128b
K_B <fengkunbin@126.com>
2018-03-30 15:42:00 +0800

    SMPTHandler is not SMTPHandler

--- config.py
+++ config.py
@@ -3,10 +3,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+	MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+	MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+		['true', 'on', '1']
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
 	FLASKY_MAIL_SENDER = 'fengkunbin@126.com'
-	FLASKY_ADMIN = 'fengkunbin@126.com'#os.environ.get('FLASKY_ADMIN')
+	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	SSL_REDIRECT = False
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 	FLASKY_POSTS_PER_PAGE = 18
@@ -24,8 +30,6 @@ class DevelopmentConfig(Config):
 	MAIL_SERVER = 'smtp.126.com'
 	MAIL_PORT = 25
 #	MAIL_USE_SSL = True
-	MAIL_USERNAME = 'fengkunbin@126.com'#os.environ.get('MAIL_USERNAME')#
-	MAIL_PASSWORD = '126mail'#os.environ.get('MAIL_PASSWORD')#
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
 		'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
@@ -45,14 +49,14 @@ class ProductionConfig(Config):
 
 		# email errors to the administrators
 		import logging
-		from logging.handlers import SMPTHandler
+		from logging.handlers import SMTPHandler
 		credentials = None
 		secure = None
 		if getattr(cls, 'MAIL_USERNAME', None) is not None:
 			credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
 			if getattr(cls, 'MAIL_USER_TLS', None):
 				secure = ()
-			mail_handler = SMPTHandler(
+			mail_handler = SMTPHandler(
 				mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
 				formaddr=cls.FLASKY_MAIL_SENDER,
 				toaddrs=[cls.FLASKY_ADMIN],
