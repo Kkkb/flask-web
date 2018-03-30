66b27e92fbdee4441fe785e087028c74a26f8428
K_B <fengkunbin@126.com>
2018-03-30 12:01:17 +0800

    Deploy command, Email notification of application errors, Heroku support with Waitress, Heroku support with Gunicorn(17a b c)

--- app/__init__.py
+++ app/__init__.py
@@ -27,7 +27,10 @@ def create_app(config_name):
 	db.init_app(app)
 	login_manager.init_app(app)
 	pagedown.init_app(app)
-	#
+	
+	if app.config['SSL_REDIRECT']:
+		from flask_sslify import flask_sslify
+		sslify = SSLify(app)
 
 	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
 	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
 	FLASKY_MAIL_SENDER = 'fengkunbin@126.com'
 	FLASKY_ADMIN = 'fengkunbin@126.com'#os.environ.get('FLASKY_ADMIN')
+	SSL_REDIRECT = False
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 	FLASKY_POSTS_PER_PAGE = 18
 	FLASKY_FOLLOWERS_PER_PAGE = 8
@@ -60,10 +61,30 @@ class ProductionConfig(Config):
 				secure=secure)
 			mail_handler.setLevel(logging.ERROR)
 			app.logger.addHandler(mail_handler)
+
+
+class HerokuConfig(ProductionConfig):
+	SSL_REDIRCT = True if os.environ.get('DYNO') else False
+
+	@classmethod
+	def init_app(cls, app):
+		ProductionConfig.init_app(app)
+
+		# handle reverse proxy server headers
+		from werkzeug.contrib.fixers import ProxyFix
+		app.wsgi_app = ProxyFix(app.wsgi_app)
+
+		# log to stderr
+		import logging
+		from logging import StreamHandler
+		file_handler = StreamHandler()
+		file_handler.setLevel(logging.WARNING)
+		app.logger.addHandler(file_handler)
+
 config = {
 	'development': DevelopmentConfig,
 	'testing': TestingConfig,
 	'production': ProductionConfig,
-
+	'heroku': HerokuConfig,
 	'default': DevelopmentConfig
 }
